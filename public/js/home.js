f6024df417fb0c3962ded2df10195fc42b8c402b
Ajith Pai <43682209+ajithpai07@users.noreply.github.com>
2020-03-07 00:49:24 +0530

    Version 2.0

--- public/js/home.js
+++ public/js/home.js
@@ -1,12 +1,12 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyD785HpCFg_dudUjcuTy66k3mQ-zNXQgA4",
-    authDomain: "takshashila-lib.firebaseapp.com",
-    databaseURL: "https://takshashila-lib.firebaseio.com",
-    projectId: "takshashila-lib",
-    storageBucket: "takshashila-lib.appspot.com",
-    messagingSenderId: "100677328500",
-    appId: "1:100677328500:web:b9c9a4bb54f7af77463b21",
-    measurementId: "G-PW6GQEYMM9"
+  apiKey: "AIzaSyBMadVVKRl84lbQ-UlHc4ynkMdjmSBEcAc",
+  authDomain: "nitte-library.firebaseapp.com",
+  databaseURL: "https://nitte-library.firebaseio.com",
+  projectId: "nitte-library",
+  storageBucket: "nitte-library.appspot.com",
+  messagingSenderId: "481374096861",
+  appId: "1:481374096861:web:6714dbe0762969f8eb3a34",
+  measurementId: "G-EGE2DG4WD6"
 };
 
 firebase.initializeApp(firebaseConfig);
